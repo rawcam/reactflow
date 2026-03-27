d497685c14cd4255b3b9b19ac3132f8eb65bad84
rawcam <rawcam@ya.ru>
2026-03-27 23:17:10 +0300

    Create DashboardPage.tsx

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,8 @@
+export const DashboardPage = () => {
+  return (
+    <div className="dashboard-wrapper">
+      <h2>ПАНЕЛЬ УПРАВЛЕНИЯ</h2>
+      <div>Здесь будут виджеты и карусель проектов</div>
+    </div>
+  )
+}
