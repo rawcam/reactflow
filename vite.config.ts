915b38f55d3586f3be5c3d0cc946786ab0089f59
rawcam <rawcam@ya.ru>
2026-04-10 09:41:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reactflow/', // имя вашего репозитория
+  base: '/reactflow/',
+  build: {
+    outDir: 'docs',   // вместо dist
+  },
 });
