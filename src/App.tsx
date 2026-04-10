9eb14b5bf628e54c0d0da7699b6c4f41ab9a95d6
rawcam <rawcam@ya.ru>
2026-04-10 09:57:59 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FlowEditorPage from './pages/FlowEditorPage';
 
 function App() {
   return (
     <BrowserRouter basename="/reactflow">
       <Routes>
-        <Route path="/" element={<Navigate to="/flow-editor" replace />} />
+        <Route path="/" element={<FlowEditorPage />} />
+        {/* Дополнительно оставляем /flow-editor для обратной совместимости */}
         <Route path="/flow-editor" element={<FlowEditorPage />} />
       </Routes>
     </BrowserRouter>
