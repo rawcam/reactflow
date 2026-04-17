ebac51b1a637a8c18d10ee580c02961101e61447
rawcam <rawcam@ya.ru>
2026-04-17 15:02:34 +0300

    Create PowerSection.tsx

---
+++ src/features/power/PowerSection.tsx
@@ -0,0 +1,21 @@
+// src/features/power/PowerSection.tsx
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setViewMode, setActiveCalculator } from '../../store/tractsSlice';
+
+export const PowerSection: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const openCalculator = () => {
+    dispatch(setActiveCalculator('power'));
+    dispatch(setViewMode('calculator'));
+  };
+
+  return (
+    <div className="section-content-inner">
+      <button className="mode-btn" onClick={openCalculator}>
+        <i className="fas fa-calculator"></i> Калькулятор питания
+      </button>
+    </div>
+  );
+};
